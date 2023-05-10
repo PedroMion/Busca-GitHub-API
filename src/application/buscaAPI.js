5d62ee5fdd019c8f80fefd427b1a959d06cfba74
Pedro Mion <pedromionbraga@gmail.com>
2023-05-10 18:50:10 -0300

    Adicionando botão de retorno

--- src/application/buscaAPI.js
+++ src/application/buscaAPI.js
@@ -1,6 +1,11 @@
 async function requestAPI(query) {
+    if(query == "") {
+        return [];
+    }
+
     const resposta = await fetch(`https://api.github.com/search/repositories?q=${query}`);
     const dados = await resposta.json();
+    
     const dadosOrdenados = dados.items.sort((a, b) => b.stargazers_count - a.stargazers_count);
 
     return dadosOrdenados;
--- src/components/Resultado.js
+++ src/components/Resultado.js
@@ -1,17 +1,23 @@
 import './Resultado.css';
 import Div100vh from 'react-div-100vh'
 import React from "react";
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 
 const Resultado = () => {
     const location = useLocation();
+    const historico = useNavigate();
     const listaResultado = location.state?.listaResultado;
     const valorLido = location.state?.valorLido;
 
-    console.log(listaResultado);
+    const retorno = () => {
+        historico("/");
+    }
 
     return(
         <div className="containerRes">
+            <div className='headerRes'>
+                <button className='buttonRes' onClick={retorno}>Voltar</button>
+            </div>
             <div className="mainRes">
                 <h1 className="titleRes">Lista de resultados para a busca {valorLido}:</h1>
                 {listaResultado && listaResultado.map((repo) => (
